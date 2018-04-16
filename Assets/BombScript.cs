5497497a591497db2c3cd2d36d6ee5e2580c7252
Hugo Ooms <hmooms@hotmail.com>
2018-04-16 15:26:54 +0200

    commit

---
+++ Assets/BombScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class BombScript : MonoBehaviour 
+{
+    public float countdown = 2f;
+	 
+	// Update is called once per frame
+	void Update () {
+        countdown -= Time.deltaTime;
+
+        if(countdown <= 0)
+        {
+            FindObjectOfType<Explosion>().Explode(transform.position); // Make this a singelton.
+            Destroy(gameObject);
+        }
+	}
+}
